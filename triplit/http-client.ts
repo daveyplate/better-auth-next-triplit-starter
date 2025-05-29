739669a72c72a1c4fef56a9cb06bb08d2705c7c3
daveycodez <daveycodez@gmail.com>
2025-05-29 14:13:40 -0700

    Update http-client.ts

--- triplit/http-client.ts
+++ triplit/http-client.ts
@@ -3,6 +3,6 @@ import { schema } from "./schema"
 
 export const httpClient = new HttpClient({
     schema,
-    serverUrl: process.env.NEXT_PUBLIC_TRIPLIT_DB_URL,
+    serverUrl: process.env.TRIPLIT_DB_URL,
     token: process.env.TRIPLIT_SERVICE_TOKEN
 })
