b3a05b650990c5ee49cdf99c0ba849e20e192bc7
daveycodez <daveycodez@gmail.com>
2025-06-01 01:54:47 -0700

    Serwist

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,18 +3,24 @@ import type { MetadataRoute } from "next"
 export default function manifest(): MetadataRoute.Manifest {
     return {
         name: "Better Auth Triplit Next.js Starter",
-        short_name: "Better Auth Starter",
-        description: "Better Auth Next.js Starter with Triplit & shadcn/ui",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#fff",
-        theme_color: "#fff",
+        short_name: "Triplit Starter",
         icons: [
             {
-                src: "/favicon.ico",
-                sizes: "any",
-                type: "image/x-icon"
+                src: "/icons/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "maskable"
+            },
+            {
+                src: "/icons/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
             }
-        ]
+        ],
+        theme_color: "#FFFFFF",
+        background_color: "#FFFFFF",
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait"
     }
 }
