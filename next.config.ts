bd19f9b7247e1669984106782c34e381930624d9
daveycodez <daveycodez@gmail.com>
2025-06-01 02:01:12 -0700

    Precache root page

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import withSerwistInit from "@serwist/next"
 import type { NextConfig } from "next"
 
+const revision = crypto.randomUUID()
+
 const withSerwist = withSerwistInit({
     swSrc: "src/app/sw.ts",
     swDest: "public/sw.js",
-    disable: process.env.NODE_ENV !== "production"
+    disable: process.env.NODE_ENV !== "production",
+    additionalPrecacheEntries: [{ url: "/", revision }]
 })
 
 const nextConfig: NextConfig = {
