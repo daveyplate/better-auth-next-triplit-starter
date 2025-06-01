f524f982bd05ca247bad70e8fd338642c5f862fe
daveycodez <daveycodez@gmail.com>
2025-05-31 21:15:08 -0700

    Update use-triplit-auth.ts

--- src/hooks/use-triplit-auth.ts
+++ src/hooks/use-triplit-auth.ts
@@ -26,7 +26,7 @@ export function useTriplitAuth({ triplit, authClient }: UseTriplitAuthOptions) {
 
             await triplit.endSession()
 
-            while (triplit.connectionStatus === "OPEN") {
+            while (triplit.connectionStatus === "OPEN" || triplit.connectionStatus === "CLOSING") {
                 await new Promise((resolve) => requestAnimationFrame(resolve))
             }
 
