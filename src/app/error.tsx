eb3226ae7d15464eaa70d3bb6584934aa355c65f
daveycodez <daveycodez@gmail.com>
2025-06-01 19:34:09 -0700

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,6 @@
 "use client" // Error boundaries must be Client Components
 
 import { useEffect } from "react"
-import SuperJSON from "superjson"
 import { Button } from "@/components/ui/button"
 
 export default function ErrorPage({
@@ -17,16 +16,10 @@ export default function ErrorPage({
 	}, [error])
 
 	return (
-		<main className="flex h-screen flex-col items-center justify-center gap-4">
+		<main className="flex grow flex-col items-center justify-center gap-4">
 			<h2 className="font-bold text-2xl">Something went wrong!</h2>
 
-			<p className="text-muted-foreground">{error.message}</p>
-
-			<div className="text-xs">{SuperJSON.stringify(error)}</div>
-
-			<Button type="button" onClick={() => reset()}>
-				Try again
-			</Button>
+			<Button onClick={() => reset()}>Try again</Button>
 		</main>
 	)
 }
