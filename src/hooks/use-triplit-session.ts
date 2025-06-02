2be184e8faeeb770c41da4c943e9cfa1faba843e
daveycodez <daveycodez@gmail.com>
2025-06-02 14:48:10 -0700

    Update use-triplit-session.ts

--- src/hooks/use-triplit-session.ts
+++ src/hooks/use-triplit-session.ts
@@ -1,18 +1,14 @@
 import { authClient } from "@/lib/auth-client"
 import { triplit } from "@/triplit/client"
 import { useConditionalQueryOne } from "./use-conditional-query"
-import { useTriplitToken } from "./use-triplit-token"
 
 export function useTriplitSession() {
 	const result = authClient.useSession()
 	const sessionData = result?.data
-	const token = useTriplitToken()
 
 	const { result: user } = useConditionalQueryOne(
 		triplit,
-		sessionData &&
-			sessionData.user.id === token?.sub &&
-			triplit.query("users").Where("id", "=", sessionData.user.id)
+		sessionData && triplit.query("users").Where("id", "=", sessionData.user.id)
 	)
 
 	if (user && sessionData) {
