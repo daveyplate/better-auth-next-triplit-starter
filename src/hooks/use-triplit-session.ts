17e390e02cd626388a22849f09be01d1a80a421a
daveycodez <daveycodez@gmail.com>
2025-06-01 18:58:09 -0700

    User not found error onRemoteFulfilled

--- src/hooks/use-triplit-session.ts
+++ src/hooks/use-triplit-session.ts
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { authClient } from "@/lib/auth-client"
 import { triplit } from "@/triplit/client"
 import { useConditionalQueryOne } from "./use-conditional-query"
@@ -9,9 +9,14 @@ export function useTriplitSession() {
 		isPending: sessionPending,
 		error: sessionError
 	} = authClient.useSession()
+	const [onRemoteFulfilled, setOnRemoteFulfilled] = useState<string>()
 	const { result: user, error: userError } = useConditionalQueryOne(
 		triplit,
-		sessionData && triplit.query("users").Where("id", "=", sessionData?.user.id)
+		sessionData &&
+			triplit.query("users").Where("id", "=", sessionData?.user.id),
+		{
+			onRemoteFulfilled: () => setOnRemoteFulfilled(sessionData?.user.id)
+		}
 	)
 
 	const result = useMemo(() => {
@@ -19,6 +24,13 @@ export function useTriplitSession() {
 			return { data: null, user: null, isPending: false, error: sessionError }
 		if (userError)
 			return { data: null, user: null, isPending: false, error: userError }
+		if (sessionData && onRemoteFulfilled === sessionData.user.id && !user)
+			return {
+				data: null,
+				user: null,
+				isPending: true,
+				error: new Error("User not found")
+			}
 		if (!user || !sessionData)
 			return { data: null, user: null, isPending: true, error: null }
 
@@ -31,7 +43,14 @@ export function useTriplitSession() {
 			isPending: false,
 			error: null
 		}
-	}, [sessionData, user, sessionError, userError, sessionPending])
+	}, [
+		sessionData,
+		user,
+		sessionError,
+		userError,
+		sessionPending,
+		onRemoteFulfilled
+	])
 
 	return result
 }
